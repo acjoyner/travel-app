6f684689820aa87da3ee584662d8ff386e39814e
acjoyner <anthony.c.joyner@gmail.com>
2023-11-21 19:50:03 -0500

    completed hero component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='border-2 border-red-500 flex flex-auto max-container padding-container relative z-30 '>
+      Footer
+    </div>
+  )
+}
+
+export default Footer;
