a2691b07d45b4a17ff3e3aac52577684fde71c7c
acjoyner <anthony.c.joyner@gmail.com>
2023-11-24 07:23:13 -0500

    Completed features section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='border-2 border-red-500 flex flex-auto max-container padding-container relative z-30 '>
-      Footer
+   <footer className='flexCenter mb-24'>
+    <div className='padding-container max-container flex w-full flex-col gap-14'>
+      <div className='flex flex-col items-start justify-center gap-{10%] md:flex-row'>
+        <Link href="/" className="mb-10" >
+          <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+        </Link>
+      </div>
     </div>
+
+   </footer>
   )
 }
 
